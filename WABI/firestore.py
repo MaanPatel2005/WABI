dfbfb9dd94c9ea53bdefd4bcc24b54c877959e14
MaanPatel2005 <maanvpatel@gmail.com>
2024-04-20 07:35:36 -0700

    firebase shit

--- WABI/firestore.py
+++ WABI/firestore.py
@@ -10,8 +10,8 @@ db = firestore.client()
 
 db.collection('users').add({'name': "Purav Patel", 'email': "puravp@uci.edu", 'weight': 140, 'height': 70, 'distance': 0, 'Kcal': 0, 'steps':0})
 db.collection('users').add({'name': "Maan Patel", 'email': "maanvp@uci.edu", 'weight': 160, 'height': 68, 'distance': 0, 'Kcal': 0, 'steps':0})
-db.collection('users').add({'name': "Dhruv Patel", 'email': "maanvp@uci.edu", 'weight': 135, 'height': 70, 'distance': 0, 'Kcal': 0, 'steps':0})
-db.collection('users').add({'name': "Maan Patel", 'email': "maanvp@uci.edu", 'weight': 160, 'height': 72, 'distance': 0, 'Kcal': 0, 'steps':0})
+db.collection('users').add({'name': "Dhruv Patel", 'email': "axiong4@uci.edu", 'weight': 135, 'height': 70, 'distance': 0, 'Kcal': 0, 'steps':0})
+db.collection('users').add({'name': "Maan Patel", 'email': "dhruvp@uci.edu", 'weight': 160, 'height': 72, 'distance': 0, 'Kcal': 0, 'steps':0})
 
 # users_ref = db.collection("users")
 # docs = users_ref.stream()
