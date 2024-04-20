890456473329b6562a4db3a8ee252b0efd6023ca
Purav Patel <puravp@uci.edu>
2024-04-20 04:12:40 -0700

    Created buddy_finder

--- WABI/pages/__init__.py
+++ WABI/pages/__init__.py
@@ -2,3 +2,5 @@ from .dashboard import dashboard
 from .index import index
 from .settings import settings
 from .leaderboard import leaderboard
+from .buddy_finder import buddy_finder
+from .chat import chat
