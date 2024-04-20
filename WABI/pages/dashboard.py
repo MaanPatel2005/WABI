14ec1c00f5e30337ebceee65b68b3c36832b348b
MaanPatel2005 <maanvpatel@gmail.com>
2024-04-19 20:51:15 -0700

    Dashboard

--- WABI/pages/dashboard.py
+++ WABI/pages/dashboard.py
@@ -1,10 +1,32 @@
 """The dashboard page."""
 
-from WABI.templates import template
+from WABI_Test.templates import template, ThemeState
 
 import reflex as rx
 
+style1 = {
+    "color": "green",
+    "font_family": "Helvetica",
+    "border_radius": "10px",
+    "background_color": ThemeState.accent_color,
+}
+style2 = {
+    "color": "black",
+    "padding": "10px",
+}
+equal_style = {
+        "width": "250px", # Assigns the basis as 0, allowing grow and shrink to control the sizing equally
+        "padding_top": "25px",
+        "padding_bottom": "25px"
+    }
 
+
+_user_name = "Wabi"
+_level = 10
+_animal = "Jaguar"
+_steps = 1000
+_distance = 10
+_calories = 800
 @template(route="/dashboard", title="Dashboard")
 def dashboard() -> rx.Component:
     """The dashboard page.
@@ -13,10 +35,28 @@ def dashboard() -> rx.Component:
         The UI for the dashboard page.
     """
     return rx.vstack(
-        rx.heading("Dashboard", size="8"),
-        rx.text("Welcome to Reflex!"),
-        rx.text(
-            "You can edit this page in ",
-            rx.code("{your_app}/pages/dashboard.py"),
+        rx.center(
+            rx.heading("Jungle Dashboard", size="8"),
+            width="100%"
         ),
+        rx.center(rx.avatar(src="/snake_head.png"), width='100%'),
+        rx.center(rx.text(f"Explorer {_user_name}"), width='100%'),
+        rx.center(rx.text(f"Lvl {_level} {_animal}"), width='100%'),
+        rx.center(
+            rx.hstack(
+                rx.box(
+                    rx.text(f"Daily Steps: {_steps} steps", style=[style1, style2, equal_style])
+                ),
+                rx.box(
+                    rx.text(f"Distance Traveled: {_distance} miles", style=[style1, style2, equal_style])
+                ),
+                rx.box(
+                    rx.text(f"Calories Burned: {_calories} calories", style=[style1, style2, equal_style])
+                ),
+                spacing="9", # Adjust the spacing as needed
+                width = "100%"
+            ),
+            width = "100%",
+        )
     )
+
