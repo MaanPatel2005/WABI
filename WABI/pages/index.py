8578c3289bd9924870d5d52769a97ea8b5a876c7
Purav Patel <puravp@uci.edu>
2024-04-19 23:50:54 -0700

    basic login stuff with local auth (works with u/p)

--- WABI/pages/index.py
+++ WABI/pages/index.py
@@ -4,9 +4,11 @@ from WABI import styles
 from WABI.templates import template
 
 import reflex as rx
+import reflex_local_auth
 
 
 @template(route="/", title="Home")
+@reflex_local_auth.require_login
 def index() -> rx.Component:
     """The home page.
 
@@ -16,3 +18,5 @@ def index() -> rx.Component:
     with open("README.md", encoding="utf-8") as readme:
         content = readme.read()
     return rx.markdown(content, component_map=styles.markdown_style)
+
+
