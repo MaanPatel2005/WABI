5a431e42aa06294f33b431508b3f6969c9327f30
Andrew Xiong <axiong4@uci.edu>
2024-04-21 02:42:35 -0700

    Co-authored-by: dhruv-patel-tech <dhruv-patel-tech@users.noreply.github.com> Co-authored-by: Purav <PatelPurav05@users.noreply.github.com>

--- WABI/pages/__init__.py
+++ WABI/pages/__init__.py
@@ -3,4 +3,5 @@ from .index import index
 from .settings import settings
 from .leaderboard import leaderboard
 from .buddy_finder import buddy_finder
+from .chat import chat
 from WABI.components.login import authenticate
